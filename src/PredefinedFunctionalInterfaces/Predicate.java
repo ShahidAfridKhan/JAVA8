4d7abc67ddea97a4653cd0396d2d33996eaf2726
ShahidAfridKhan <shahidafridkhanphatan@gmail.com>
2026-07-01 10:40:24 +0530

    this is predefined funactional interfaces

---
+++ src/PredefinedFunctionalInterfaces/Predicate.java
@@ -0,0 +1,14 @@
+package PredefinedFunctionalInterfaces;
+import java.util.*;
+
+interface Cal{
+    int add(int a,int b);
+}
+
+public class Predicate {
+    public static void main(String[] args) {
+
+        Cal ad=(a,b)->a+b;
+        System.out.println(ad.add(10,30));
+    }
+}
